df217ba95d4e7923968752fdcd521b8bf1237272
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-04-23 16:25:19 -0400

    Solved it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,67 @@
+import { dogsData } from "./data";
+import { useState } from "react";
+import DogDetails from "./DogDetails";
+
 function App() {
+  const [dogs, setDogs] = useState(dogsData);
+
+  function addDog() {
+    const rover = {
+      id: dogs.length + 50,
+      name: "Rover",
+      present: false,
+      grade: "100",
+      notes: "The goodest new dog",
+    };
+    setDogs([rover, ...dogs]);
+  }
+
+  function removeDog(dogID) {
+    // use the filter method to remove any dogs that have a matching id
+    const filteredDogArray = dogs.filter((dog) => dog.id !== dogID);
+    // set the dogs array to the new array that will not have the removed dog
+    setDogs(filteredDogArray);
+  }
+
+  function updateDogAttendance(dogId) {
+    // Copy the dogs array so that the copy can be updated
+    const dogArray = [...dogs];
+    // Find the dog with the matching id number's array position
+    const index = dogArray.findIndex((dog) => dogId === dog.id);
+    // Access the dog's present property and update the value
+    // By using ! it will toggle the value of present
+    dogArray[index].present = !dogArray[index].present;
+    // Put the updated array into setDogs to update the dogs array
+    setDogs(dogArray);
+  }
+
   return (
     <div className="App">
       <header>
         <h1> Bark and Bowl Doggy Day Care</h1>
         <h2>{new Date().toDateString()}</h2>
+        <button onClick={addDog}>Add a new dog</button>
+        <ul>
+        {
+          dogs.map((dog) => {
+            return (
+              <li key={dog.id}>
+                <span
+                onClick={() => updateDogAttendance(dog.id)}
+                style={
+                  dog.present ?
+                  { textDecoration: "none" } :
+                  { textDecoration: "line-through" }
+                }
+                >{dog.name}{" "}</span>
+                <button onClick={() => removeDog(dog.id)}>remove</button>
+                <DogDetails dog={dog} />
+              </li>
+            );
+          })
+        }
+        </ul>
+
       </header>
       <aside></aside>
       <main></main>
